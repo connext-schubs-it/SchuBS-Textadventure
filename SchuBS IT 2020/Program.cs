444e451b48f34bb31aeff84cdd625423a0732736
Klose, Nils <nils.klose@connext.de>
2020-04-06 13:36:13 +0200

    Oberfläche

--- SchuBS IT 2020/Program.cs
+++ SchuBS IT 2020/Program.cs
@@ -16,7 +16,7 @@ namespace SchuBS_IT_2020
       {
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault(false);
-         Application.Run(new Form1());
+         Application.Run(new Textadventure());
       }
    }
 }
---
+++ SchuBS IT 2020/Textadventure.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace SchuBS_IT_2020
+{
+   partial class Textadventure
+   {
+      /// <summary>
+      /// Erforderliche Designervariable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Verwendete Ressourcen bereinigen.
+      /// </summary>
+      /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Vom Windows Form-Designer generierter Code
+
+      /// <summary>
+      /// Erforderliche Methode für die Designerunterstützung.
+      /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+         this.richTextBoxHauptText = new System.Windows.Forms.RichTextBox();
+         this.labelGesundheit = new System.Windows.Forms.Label();
+         this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+         this.textBoxEingabe = new System.Windows.Forms.TextBox();
+         this.buttonZurueck = new System.Windows.Forms.Button();
+         this.groupBoxInventar = new System.Windows.Forms.GroupBox();
+         this.flowLayoutPanelInventar = new System.Windows.Forms.FlowLayoutPanel();
+         this.labelLokalisation = new System.Windows.Forms.Label();
+         this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+         this.button1 = new System.Windows.Forms.Button();
+         this.button2 = new System.Windows.Forms.Button();
+         this.button3 = new System.Windows.Forms.Button();
+         this.tableLayoutPanel1.SuspendLayout();
+         this.tableLayoutPanel2.SuspendLayout();
+         this.groupBoxInventar.SuspendLayout();
+         this.tableLayoutPanel3.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // tableLayoutPanel1
+         // 
+         this.tableLayoutPanel1.AutoSize = true;
+         this.tableLayoutPanel1.ColumnCount = 1;
+         this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+         this.tableLayoutPanel1.Controls.Add(this.richTextBoxHauptText, 0, 0);
+         this.tableLayoutPanel1.Controls.Add(this.labelGesundheit, 0, 1);
+         this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+         this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+         this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+         this.tableLayoutPanel1.RowCount = 3;
+         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+         this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 528);
+         this.tableLayoutPanel1.TabIndex = 5;
+         // 
+         // richTextBoxHauptText
+         // 
+         this.richTextBoxHauptText.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.richTextBoxHauptText.Location = new System.Drawing.Point(3, 3);
+         this.richTextBoxHauptText.Name = "richTextBoxHauptText";
+         this.richTextBoxHauptText.Size = new System.Drawing.Size(794, 409);
+         this.richTextBoxHauptText.TabIndex = 1;
+         this.richTextBoxHauptText.Text = "";
+         // 
+         // labelGesundheit
+         // 
+         this.labelGesundheit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+         this.labelGesundheit.AutoSize = true;
+         this.labelGesundheit.Location = new System.Drawing.Point(3, 415);
+         this.labelGesundheit.Name = "labelGesundheit";
+         this.labelGesundheit.Size = new System.Drawing.Size(61, 13);
+         this.labelGesundheit.TabIndex = 1;
+         this.labelGesundheit.Text = "Gesundheit";
+         this.labelGesundheit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+         // 
+         // tableLayoutPanel2
+         // 
+         this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.tableLayoutPanel2.AutoSize = true;
+         this.tableLayoutPanel2.ColumnCount = 3;
+         this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+         this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+         this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+         this.tableLayoutPanel2.Controls.Add(this.textBoxEingabe, 0, 1);
+         this.tableLayoutPanel2.Controls.Add(this.buttonZurueck, 1, 1);
+         this.tableLayoutPanel2.Controls.Add(this.groupBoxInventar, 2, 0);
+         this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+         this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 431);
+         this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+         this.tableLayoutPanel2.RowCount = 2;
+         this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+         this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+         this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 94);
+         this.tableLayoutPanel2.TabIndex = 2;
+         // 
+         // textBoxEingabe
+         // 
+         this.textBoxEingabe.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+         this.textBoxEingabe.Location = new System.Drawing.Point(3, 54);
+         this.textBoxEingabe.Name = "textBoxEingabe";
+         this.textBoxEingabe.Size = new System.Drawing.Size(346, 20);
+         this.textBoxEingabe.TabIndex = 4;
+         // 
+         // buttonZurueck
+         // 
+         this.buttonZurueck.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+         this.buttonZurueck.AutoSize = true;
+         this.buttonZurueck.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+         this.buttonZurueck.Location = new System.Drawing.Point(355, 38);
+         this.buttonZurueck.Name = "buttonZurueck";
+         this.buttonZurueck.Size = new System.Drawing.Size(84, 53);
+         this.buttonZurueck.TabIndex = 5;
+         this.buttonZurueck.Text = "Schritt-Zurück";
+         this.buttonZurueck.UseVisualStyleBackColor = true;
+         // 
+         // groupBoxInventar
+         // 
+         this.groupBoxInventar.AutoSize = true;
+         this.groupBoxInventar.Controls.Add(this.flowLayoutPanelInventar);
+         this.groupBoxInventar.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.groupBoxInventar.Location = new System.Drawing.Point(445, 3);
+         this.groupBoxInventar.Name = "groupBoxInventar";
+         this.tableLayoutPanel2.SetRowSpan(this.groupBoxInventar, 2);
+         this.groupBoxInventar.Size = new System.Drawing.Size(346, 88);
+         this.groupBoxInventar.TabIndex = 6;
+         this.groupBoxInventar.TabStop = false;
+         this.groupBoxInventar.Text = "Inventar";
+         // 
+         // flowLayoutPanelInventar
+         // 
+         this.flowLayoutPanelInventar.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.flowLayoutPanelInventar.Location = new System.Drawing.Point(3, 16);
+         this.flowLayoutPanelInventar.Name = "flowLayoutPanelInventar";
+         this.flowLayoutPanelInventar.Size = new System.Drawing.Size(340, 69);
+         this.flowLayoutPanelInventar.TabIndex = 7;
+         // 
+         // labelLokalisation
+         // 
+         this.labelLokalisation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+         this.labelLokalisation.AutoSize = true;
+         this.labelLokalisation.Location = new System.Drawing.Point(731, 6);
+         this.labelLokalisation.Name = "labelLokalisation";
+         this.labelLokalisation.Size = new System.Drawing.Size(63, 13);
+         this.labelLokalisation.TabIndex = 6;
+         this.labelLokalisation.Text = "Lokalisation";
+         // 
+         // tableLayoutPanel3
+         // 
+         this.tableLayoutPanel3.AutoSize = true;
+         this.tableLayoutPanel3.ColumnCount = 3;
+         this.tableLayoutPanel2.SetColumnSpan(this.tableLayoutPanel3, 2);
+         this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+         this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+         this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+         this.tableLayoutPanel3.Controls.Add(this.button1, 0, 0);
+         this.tableLayoutPanel3.Controls.Add(this.button2, 1, 0);
+         this.tableLayoutPanel3.Controls.Add(this.button3, 2, 0);
+         this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+         this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+         this.tableLayoutPanel3.RowCount = 1;
+         this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+         this.tableLayoutPanel3.Size = new System.Drawing.Size(436, 29);
+         this.tableLayoutPanel3.TabIndex = 3;
+         // 
+         // button1
+         // 
+         this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.button1.Location = new System.Drawing.Point(3, 3);
+         this.button1.Name = "button1";
+         this.button1.Size = new System.Drawing.Size(139, 23);
+         this.button1.TabIndex = 0;
+         this.button1.Text = "Button 1";
+         this.button1.UseVisualStyleBackColor = true;
+         // 
+         // button2
+         // 
+         this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.button2.Location = new System.Drawing.Point(148, 3);
+         this.button2.Name = "button2";
+         this.button2.Size = new System.Drawing.Size(139, 23);
+         this.button2.TabIndex = 1;
+         this.button2.Text = "Button 2";
+         this.button2.UseVisualStyleBackColor = true;
+         // 
+         // button3
+         // 
+         this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.button3.Location = new System.Drawing.Point(293, 3);
+         this.button3.Name = "button3";
+         this.button3.Size = new System.Drawing.Size(140, 23);
+         this.button3.TabIndex = 2;
+         this.button3.Text = "Button 3";
+         this.button3.UseVisualStyleBackColor = true;
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(800, 528);
+         this.Controls.Add(this.labelLokalisation);
+         this.Controls.Add(this.tableLayoutPanel1);
+         this.MinimumSize = new System.Drawing.Size(320, 250);
+         this.Name = "Form1";
+         this.Text = "Textadventure";
+         this.tableLayoutPanel1.ResumeLayout(false);
+         this.tableLayoutPanel1.PerformLayout();
+         this.tableLayoutPanel2.ResumeLayout(false);
+         this.tableLayoutPanel2.PerformLayout();
+         this.groupBoxInventar.ResumeLayout(false);
+         this.tableLayoutPanel3.ResumeLayout(false);
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox richTextBoxHauptText;
+        private System.Windows.Forms.Label labelGesundheit;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox textBoxEingabe;
+        private System.Windows.Forms.Button buttonZurueck;
+        private System.Windows.Forms.Label labelLokalisation;
+        private System.Windows.Forms.GroupBox groupBoxInventar;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelInventar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
