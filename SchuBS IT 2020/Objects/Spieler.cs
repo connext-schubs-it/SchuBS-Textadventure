3a13dabed30bd32ae29453c26696655b5ec891dc
HainsleyArriott <jona.deimel@connext.de>
2020-04-07 09:10:52 +0200

    Zu Klasse Spieler: Lebenspunkte, Level, Inventar. Bild für ersten Szene eingefügt

--- SchuBS IT 2020/Objects/Spieler.cs
+++ SchuBS IT 2020/Objects/Spieler.cs
@@ -4,5 +4,8 @@
     {
         public string Name { get; set; }
         public Klasse Klasse { get; set; } = Klasse.GetByKlassenTyp(KlassenTyp.Keine);
+        public int Lebenspunkte { get; set; }
+        public int Level { get; set; }
+        public string[] Inventar { get; set; }
     }
 }
