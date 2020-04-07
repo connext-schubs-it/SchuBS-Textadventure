2655ecd0a446c051894647b2384efef48a6ca60a
Gehlhaus, Anna <Anna.Gehlhaus@connext.de>
2020-04-07 10:48:13 +0200

    Anfang der Story 2

--- SchuBS IT 2020/Story.cs
+++ SchuBS IT 2020/Story.cs
@@ -8,11 +8,16 @@ namespace SchuBS_IT_2020
 {
     public partial class Textadventure
     {
+        #region Variablen
+
+        private int previous = -1;
+
+        #endregion
         private void button1_Click(object sender, EventArgs e)
         {
-            switch (this.button1.Text)
+            switch (previous)
             {
-                case "Augen öffnen":
+                case 0:
                     AugenOeffnen();
                     break;
                 default:
@@ -22,9 +27,9 @@ namespace SchuBS_IT_2020
 
         private void button2_Click(object sender, EventArgs e)
         {
-            switch (this.button2.Text)
+            switch (previous)
             {
-                case "Augen geschlossen lassen":
+                case 0:
                     AugenGeschlossen();
                     break;
                 default:
@@ -34,7 +39,7 @@ namespace SchuBS_IT_2020
 
         private void button3_Click(object sender, EventArgs e)
         {
-            switch (this.button3.Text)
+            switch (previous)
             {
                 default:
                     break;
@@ -52,6 +57,7 @@ namespace SchuBS_IT_2020
                 "Ihr fühlt keinen Schmerz, alles scheint wie immer und doch könnt Ihr Euch an nichts mehr erinnern...", "");
 
             SetButtonsText(new string[]{"Augen öffnen", "Augen geschlossen lassen"});
+            previous = 0;
         }
 
         private void AugenOeffnen()
@@ -60,11 +66,13 @@ namespace SchuBS_IT_2020
                 "Plötzlich fangt ihr an etwas zu erkennen. Eine Gestalt befindet sich auf dem Feldweg und bewegt sich in eure Richtung, zuerst langsam, doch als sie euch bemerkt, beginnt sie schneller zu laufen. Nun steht ein fremder Mann vor euch: ",
                 "Fremder Mann: 'Ein Mittagsschlaf hier unter der prallen Sonne? Recht ungewöhnlich für diese Gegend...",
                 "Verratet ihr mir euren Namen?'", "Hallo Fremder. Wie ist dein Name?");
+            previous = 1;
         }
 
         private void AugenGeschlossen()
         {
             WriteText("Euer ungestillter Durst nach Abenteuern führt zum unweigerlichen Ende.", "");
+            previous = 2;
         }
     }
 }
