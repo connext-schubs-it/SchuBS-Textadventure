2b3fc3d1f9d202cbfd86117cc517e0f2abbed900
Gehlhaus, Anna <Anna.Gehlhaus@connext.de>
2020-04-08 08:59:38 +0200

    neue Datein für Macht und Reichtum

---
+++ SchuBS-Textadventure/Story/Macht.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using SchuBS_IT_2020;
+using SchuBS_Textadventure.Objects;
+
+namespace SchuBS_Textadventure
+{
+    public partial class Textadventure
+    {
+
+    }
+}
